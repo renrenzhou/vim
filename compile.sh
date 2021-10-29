5e5927e3b4a0a24b648b1595b9765ec95380bafa
hjzhou <itjustzhou@gmail.com>
2021-10-29 23:07:52 +0800

    fix bug

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 ./configure --with-features=huge \
 	        --enable-multibyte \
             --enable-rubyinterp \
+            --enable-python3interp \
+            --with-python3-config-dir=/usr/lib/python3.7/config-3.7m-aarch64-linux-gnu \
             --enable-pythoninterp \
             --with-python-config-dir=/usr/lib/python2.7/config-aarch64-linux-gnu \
             --enable-perlinterp \
