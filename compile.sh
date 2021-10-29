13505fae0b87489a0eb95b4d921c9859c9cde226
hjzhou <itjustzhou@gmail.com>
2021-10-29 22:52:13 +0800

    add vim compile command

---
+++ compile.sh
@@ -0,0 +1,8 @@
+./configure --with-features=huge \
+	        --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config-aarch64-linux-gnu \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-cscope --prefix=/usr
